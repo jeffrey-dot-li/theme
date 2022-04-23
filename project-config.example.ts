7e3bec230a5a56edc63050e64b13aeb0ea450949
Jeffrey Li <dr.cheddar@outlook.com>
2022-04-23 14:50:35 -0400

    org: add example config file

---
+++ project-config.example.ts
@@ -0,0 +1,7 @@
+import { googleFonts, localFonts } from './google-fonts';
+export const projectFonts
+= {
+  content: googleFonts.Quicksand,
+  banner: localFonts['Pesta Stencil'],
+  title: googleFonts.Oswald,
+} as const;
